80e22c8cc2b24692021618d532caf02dc6ad33c8
jecko2 <89352917+jecko2@users.noreply.github.com>
2021-08-28 11:10:29 +0300

    Add files via upload
    Initial commit for online shop.

---
+++ onlineresturant/main.js
@@ -0,0 +1,30 @@
+// responsiveness habdling on the menu toggle
+
+let menu = document.querySelector("#menu-bars");
+let navbar = document.querySelector(".navbar");
+
+
+menu.onclick = () =>{
+    menu.classList.toggle("fa-times");
+    navbar.classList.toggle("active");
+}
+
+window.onscroll = () =>{
+    menu.classList.remove("fa-times");
+    navbar.classList.remove("active");
+}
+
+
+document.querySelector("#search-icon").onclick = () => {
+    document.querySelector("#search-form").classList.toggle("active");
+
+}
+document.querySelector("#close").onclick = () => {
+    document.querySelector("#search-form").classList.remove("active");
+}
+
+
+
+// function menuClick() {
+//     menu.classList.toggle("fa-times")
+// }
